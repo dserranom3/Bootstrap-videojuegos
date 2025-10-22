4992610ad06f05ad05c160e046a09f5c93e31d58
Daniel6290 <dserranom@ucenfotec.ac.cr>
2025-10-22 17:16:52 -0600

    Pimer avance del HTML
    Primer avance creado con templates

---
+++ script.js
@@ -0,0 +1,4 @@
+// Al hacer clic en el botón de noticias
+document.getElementById("btnNoticia").addEventListener("click", function() {
+  alert("¡Próximamente más detalles acerca de su remasterizacion!");
+});
